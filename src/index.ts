1386a1de2a79d7edc591102ea55bc69d2beffe9c
Michael Ficarra <mficarra@shapesecurity.com>
2023-11-25 11:26:02 -0700

    change concat to take a single parameter

--- src/index.ts
+++ src/index.ts
@@ -23,20 +23,23 @@ function isObject(obj: unknown): obj is Object {
 
 type IteratorOrIterable<A> = Iterable<A> | Iterator<A>
 
-function concatImpl<A>(...iterators: Array<IteratorOrIterable<A>>): Generator<A>
-function concatImpl<A, B>(iteratorA: IteratorOrIterable<A>, iteratorB: IteratorOrIterable<B>): Generator<A | B>
-function concatImpl<A, B, C>(iteratorA: IteratorOrIterable<A>, iteratorB: IteratorOrIterable<B>, iteratorC: IteratorOrIterable<C>): Generator<A | B | C>
-function concatImpl<A, B, C, D>(iteratorA: IteratorOrIterable<A>, iteratorB: IteratorOrIterable<B>, iteratorC: IteratorOrIterable<C>, iteratorD: IteratorOrIterable<D>): Generator<A | B | C | D>
-function concatImpl<A, B, C, D, E>(iteratorA: IteratorOrIterable<A>, iteratorB: IteratorOrIterable<B>, iteratorC: IteratorOrIterable<C>, iteratorD: IteratorOrIterable<D>, iteratorE: IteratorOrIterable<E>): Generator<A | B | C | D | E>
-function concatImpl(...iterators: Array<IteratorOrIterable<unknown>>): Generator<unknown>
-function* concatImpl(...iterators: Array<unknown>): Generator<unknown> {
-  for (const iter of iterators) {
-    yield* { [Symbol.iterator]() { return getIteratorFlattenable(iter, 'reject-strings'); } };
+function liftIterator<A>(iter: Iterator<A, unknown, unknown>): Iterable<A> {
+  return { [Symbol.iterator]() { return iter; } };
+}
+
+function concatImpl<A>(iterators: IteratorOrIterable<IteratorOrIterable<A>>): Generator<A>
+function concatImpl(iterators: IteratorOrIterable<IteratorOrIterable<unknown>>): Generator<unknown>
+function* concatImpl(iterators: IteratorOrIterable<IteratorOrIterable<unknown>>): Generator<unknown> {
+  for (const iter of liftIterator(getIteratorFlattenable(iterators, 'reject-strings'))) {
+    // TODO: I don't know why TypeScript won't let me use yield* here
+    for (const v of liftIterator(getIteratorFlattenable(iter, 'reject-strings'))) {
+      yield v;
+    }
   }
 }
 
 // NOTE: this line makes concat non-constructible, and gives it the appropriate name and length
-const concat = (...iterators: Array<any>) => concatImpl(...iterators);
+const concat = (iterators: IteratorOrIterable<IteratorOrIterable<unknown>>) => concatImpl(iterators);
 
 Object.defineProperty(Iterator, 'concat', {
   configurable: true,
--- test/index.mjs
+++ test/index.mjs
@@ -4,10 +4,10 @@ import '../lib/index.js';
 
 test('basic', async t => {
   assert.deepEqual(
-    Array.from(Iterator.concat(
+    Array.from(Iterator.concat([
       [0, 1, 2],
       [3, 4, 5],
-    )),
+    ])),
     [0, 1, 2, 3, 4, 5],
   );
 });
