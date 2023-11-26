2b7e07e1ae9579ea6a1a277061527bf8729ef731
Michael Ficarra <mficarra@shapesecurity.com>
2023-11-25 18:26:28 -0700

    add from

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,9 @@ if (typeof Iterator === 'undefined' || Iterator == null) {
   globalThis.Iterator = function() {};
 }
 
-function getIteratorFlattenable(obj: any, stringHandling: 'iterate-strings' | 'reject-strings'): Iterator<unknown, unknown, unknown> {
+function getIteratorFlattenable<A>(obj: IteratorOrIterable<A>, stringHandling: 'iterate-strings' | 'reject-strings'): Iterator<A>
+function getIteratorFlattenable(obj: any, stringHandling: 'iterate-strings' | 'reject-strings'): Iterator<unknown>
+function getIteratorFlattenable(obj: any, stringHandling: 'iterate-strings' | 'reject-strings'): Iterator<unknown> {
   if (Object(obj) !== obj) {
     if (stringHandling === 'reject-strings' || typeof obj != 'string') {
       throw new TypeError;
@@ -22,19 +24,33 @@ function isObject(obj: unknown): obj is Object {
 }
 
 type IteratorOrIterable<A> = Iterable<A> | Iterator<A>
+interface Iterable<T> {
+    [Symbol.iterator](): Iterator<T, unknown, unknown>;
+}
 
-function liftIterator<A>(iter: Iterator<A, unknown, unknown>): Iterable<A> {
+function liftIterator<A>(iter: Iterator<A>): Iterable<A> {
   return { [Symbol.iterator]() { return iter; } };
 }
 
 function concatImpl<A>(iterators: IteratorOrIterable<IteratorOrIterable<A>>): Generator<A>
 function concatImpl(iterators: IteratorOrIterable<IteratorOrIterable<unknown>>): Generator<unknown>
-function* concatImpl(iterators: IteratorOrIterable<IteratorOrIterable<unknown>>): Generator<unknown> {
+function* concatImpl(iterators: unknown): Generator<unknown> {
   for (const iter of liftIterator(getIteratorFlattenable(iterators, 'reject-strings'))) {
-    // TODO: I don't know why TypeScript won't let me use yield* here
-    for (const v of liftIterator(getIteratorFlattenable(iter, 'reject-strings'))) {
-      yield v;
-    }
+    yield* liftIterator(getIteratorFlattenable(iter, 'reject-strings'));
+  }
+}
+
+function fromImpl<A>(...iterators: Array<IteratorOrIterable<A>>): Generator<A>
+function fromImpl(): Generator<never>
+function fromImpl<A>(iteratorA: IteratorOrIterable<A>): Generator<A>
+function fromImpl<A, B>(iteratorA: IteratorOrIterable<A>, iteratorB: IteratorOrIterable<B>): Generator<A | B>
+function fromImpl<A, B, C>(iteratorA: IteratorOrIterable<A>, iteratorB: IteratorOrIterable<B>, iteratorC: IteratorOrIterable<C>): Generator<A | B | C>
+function fromImpl<A, B, C, D>(iteratorA: IteratorOrIterable<A>, iteratorB: IteratorOrIterable<B>, iteratorC: IteratorOrIterable<C>, iteratorD: IteratorOrIterable<D>): Generator<A | B | C | D>
+function fromImpl<A, B, C, D, E>(iteratorA: IteratorOrIterable<A>, iteratorB: IteratorOrIterable<B>, iteratorC: IteratorOrIterable<C>, iteratorD: IteratorOrIterable<D>, iteratorE: IteratorOrIterable<E>): Generator<A | B | C | D | E>
+function fromImpl(...iterators: Array<IteratorOrIterable<unknown>>): Generator<unknown>
+function* fromImpl(...iterators: Array<unknown>): Generator<unknown> {
+  for (const iter of iterators) {
+    yield* liftIterator(getIteratorFlattenable(iter, 'reject-strings'));
   }
 }
 
@@ -46,4 +62,14 @@ Object.defineProperty(Iterator, 'concat', {
   writable: true,
   enumerable: false,
   value: concat,
+});
+
+// NOTE: this line makes from non-constructible, and gives it the appropriate name and length
+const from = (...iterators: Array<IteratorOrIterable<unknown>>) => fromImpl(...iterators);
+
+Object.defineProperty(Iterator, 'from', {
+  configurable: true,
+  writable: true,
+  enumerable: false,
+  value: from,
 });
