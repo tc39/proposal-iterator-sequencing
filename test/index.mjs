5bdba284aad04be57aaf8f27fdfc2059ab7b661b
Michael Ficarra <mficarra@shapesecurity.com>
2023-12-11 09:39:37 -0700

    disable concat in prototype

--- test/index.mjs
+++ test/index.mjs
@@ -2,25 +2,25 @@ import { test } from 'node:test';
 import * as assert from 'node:assert/strict';
 import '../lib/index.js';
 
-test('concat', async t => {
-  assert.deepEqual(
-    Array.from(Iterator.concat([])),
-    [],
-  );
-  assert.deepEqual(
-    Array.from(Iterator.concat([
-      [0, 1, 2],
-    ])),
-    [0, 1, 2],
-  );
-  assert.deepEqual(
-    Array.from(Iterator.concat([
-      [0, 1, 2],
-      [3, 4, 5],
-    ])),
-    [0, 1, 2, 3, 4, 5],
-  );
-});
+// test('concat', async t => {
+//   assert.deepEqual(
+//     Array.from(Iterator.concat([])),
+//     [],
+//   );
+//   assert.deepEqual(
+//     Array.from(Iterator.concat([
+//       [0, 1, 2],
+//     ])),
+//     [0, 1, 2],
+//   );
+//   assert.deepEqual(
+//     Array.from(Iterator.concat([
+//       [0, 1, 2],
+//       [3, 4, 5],
+//     ])),
+//     [0, 1, 2, 3, 4, 5],
+//   );
+// });
 
 test('from', async t => {
   assert.deepEqual(
